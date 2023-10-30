7e229926081e89c124cf8b3a0bd81c19d234dd63
Mike Bland <mbland@acm.org>
2023-10-30 12:10:27 -0400

    Extract testing.utils.Docker
    Again, these are generally reusable utilities.

--- strcalc/src/test/java/com/mike_bland/training/testing/tomcat/LocalTomcatServer.java
+++ strcalc/src/test/java/com/mike_bland/training/testing/tomcat/LocalTomcatServer.java
@@ -1,9 +1,8 @@
 package com.mike_bland.training.testing.tomcat;
 
-import com.mike_bland.training.testing.utils.GitRepository;
+import com.mike_bland.training.testing.utils.Docker;
 import com.mike_bland.training.testing.utils.PortPicker;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URI;
 
@@ -15,12 +14,12 @@ public class LocalTomcatServer {
     private Process runCmd;
 
     public synchronized URI start() throws IOException, InterruptedException {
-        assertDockerIsAvailable();
+        Docker.assertIsAvailable();
 
         if (!running) {
-            imageId = createTemporaryImage(DOCKERFILE);
+            imageId = Docker.createTemporaryImage(DOCKERFILE);
             port = PortPicker.pickUnusedPort();
-            runCmd = runTemporaryImage(imageId, port + ":8080");
+            runCmd = Docker.runImage(imageId, port + ":8080");
             Thread.sleep(1000);
             running = true;
         }
@@ -31,74 +30,8 @@ public class LocalTomcatServer {
         if (running) {
             runCmd.destroy();
             Thread.sleep(1000);
-            destroyTemporaryImage(imageId);
+            Docker.destroyImage(imageId);
             running = false;
-        }
-    }
-
-    void assertDockerIsAvailable() throws IOException {
-        try {
-            Process dockerInfo = new ProcessBuilder("docker", "info")
-                    .start();
-
-            try (BufferedReader stderr = dockerInfo.errorReader()) {
-                if (dockerInfo.waitFor() != 0) {
-                    throw new IOException(stderr.readLine());
-                }
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new IOException("Docker not available: " + e);
-        }
-    }
-
-    String createTemporaryImage(String dockerfile) throws IOException {
-        try {
-            Process dockerBuild = new ProcessBuilder(
-                    "docker", "build", "-q", "-f", dockerfile, ".")
-                    .directory(GitRepository.getRoot())
-                    .start();
-
-            try (BufferedReader stdout = dockerBuild.inputReader();
-                 BufferedReader stderr = dockerBuild.errorReader()) {
-                if (dockerBuild.waitFor() == 0) {
-                    return stdout.readLine();
-                }
-                throw new IOException(stderr.readLine());
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new IOException(
-                    "failed to create temporary Docker image: " + e
-            );
-        }
-    }
-
-    Process runTemporaryImage(String imageId, String portMap)
-            throws IOException {
-        try {
-            return new ProcessBuilder(
-                    "docker", "run", "--rm", "-p", portMap, imageId)
-                    .start();
-
-        } catch (IOException e) {
-            throw new IOException("error running Docker image: " + e);
-        }
-    }
-
-    void destroyTemporaryImage(String imageId) throws IOException {
-        try {
-            Process dockerRmi = new ProcessBuilder(
-                    "docker", "rmi", imageId)
-                    .start();
-
-            try (BufferedReader stderr = dockerRmi.errorReader()) {
-                if (dockerRmi.waitFor() != 0) {
-                    throw new IOException(stderr.readLine());
-                }
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new IOException(
-                    "failed to destroy temporary Docker image: " + e
-            );
         }
     }
 }
---
+++ strcalc/src/test/java/com/mike_bland/training/testing/utils/Docker.java
@@ -0,0 +1,71 @@
+package com.mike_bland.training.testing.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Docker {
+    public static void assertIsAvailable() throws IOException {
+        try {
+            Process dockerInfo = new ProcessBuilder("docker", "info")
+                    .start();
+
+            try (BufferedReader stderr = dockerInfo.errorReader()) {
+                if (dockerInfo.waitFor() != 0) {
+                    throw new IOException(stderr.readLine());
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException("Docker not available: " + e);
+        }
+    }
+
+    public static String createTemporaryImage(String dockerfile)
+            throws IOException {
+        try {
+            Process dockerBuild = new ProcessBuilder(
+                    "docker", "build", "-q", "-f", dockerfile, ".")
+                    .directory(GitRepository.getRoot())
+                    .start();
+
+            try (BufferedReader stdout = dockerBuild.inputReader();
+                 BufferedReader stderr = dockerBuild.errorReader()) {
+                if (dockerBuild.waitFor() == 0) {
+                    return stdout.readLine();
+                }
+                throw new IOException(stderr.readLine());
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException(
+                    "failed to create temporary Docker image: " + e
+            );
+        }
+    }
+
+    public static Process runImage(String imageId, String portMap)
+            throws IOException {
+        try {
+            return new ProcessBuilder(
+                    "docker", "run", "--rm", "-p", portMap, imageId)
+                    .start();
+
+        } catch (IOException e) {
+            throw new IOException("error running Docker image: " + e);
+        }
+    }
+
+    public static void destroyImage(String imageId) throws IOException {
+        try {
+            Process dockerRmi = new ProcessBuilder(
+                    "docker", "rmi", imageId)
+                    .start();
+
+            try (BufferedReader stderr = dockerRmi.errorReader()) {
+                if (dockerRmi.waitFor() != 0) {
+                    throw new IOException(stderr.readLine());
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new IOException("failed to destroy Docker image: " + e);
+        }
+    }
+}
