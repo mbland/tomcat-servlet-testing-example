52eb200440bb5dba22ab59ecccbdf0cdf62a7eca
Mike Bland <mbland@acm.org>
2023-11-06 17:00:42 -0500

    Add empty large WebApplicationTest
    The continuous integration build just failed because I removed the
    original LargePlaceholderTest.
    
    - https://github.com/mbland/tomcat-servlet-testing-example/actions/runs/6776994475/job/18419713640
    
    It passed locally because I had a proof of concept WebApplicationTest
    using Selenium WebDriver that I hadn't checked in yet.
    
    I plan to check a proper WebApplicationTest implementation soon, but for
    now, I'm committing an empty placeholder to get the build passing again.

---
+++ strcalc/src/test/java/com/mike_bland/training/testing/stringcalculator/WebApplicationTest.java
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.mike_bland.training.testing.stringcalculator;
+
+import com.mike_bland.training.testing.annotations.LargeTest;
+
+public class WebApplicationTest {
+    // This placeholder test exists solely to allow the Gradle "test" task to
+    // pass until actual @SmallTests are present:
+    //
+    // - https://docs.gradle.org/8.4/userguide/upgrading_version_8.html#test_task_fail_on_no_test_executed
+    //
+    //  Please replace or delete it when you're ready to add actual tests.
+    @LargeTest
+    void testPlaceholder() {
+    }
+}
