469ca9d176aaa078ce01db49e5c25d4f89063bc2
Mike Bland <mbland@acm.org>
2023-12-08 20:14:42 -0500

    frontend: s/let/const/g
    Since `const` means the variable won't get reassigned, let's use it
    everywhere.

--- strcalc/src/main/frontend/main.test.js
+++ strcalc/src/main/frontend/main.test.js
@@ -8,15 +8,15 @@ import { describe, afterEach, expect, test } from 'vitest'
 import { PageLoader } from './test-helpers.js'
 
 describe('String Calculator UI', () => {
-  let loader = new PageLoader('/strcalc')
+  const loader = new PageLoader('/strcalc')
 
   afterEach(() => loader.closeAll())
 
   describe('initial state', () => {
     test('contains the "Hello, World!" placeholder', async () => {
-      let { document } = await loader.load('index.html')
+      const { document } = await loader.load('index.html')
 
-      let e = document.querySelector('#app .placeholder a')
+      const e = document.querySelector('#app .placeholder a')
 
       expect(e.textContent).toContain('Hello, World!')
       expect(e.href).toContain('%22Hello,_World!%22')
