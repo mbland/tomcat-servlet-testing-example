84132e537a6b4652da7267aedb72ee7a94f43b2e
Mike Bland <mbland@acm.org>
2024-02-28 21:35:54 -0500

    Add an empty ProdStringCalculator class
    This is the third step in the JavaScript Test-Driven Development demo.
    
    "ProdStringCalculatorTest > empty string returns zero" now successfully
    builds and executes.
    
    Now we're ready to break the test again by calling the nonexistent
    calc.add() method.

--- strcalc/src/main/frontend/components/prod-calculator.test.js
+++ strcalc/src/main/frontend/components/prod-calculator.test.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-
+import ProdStringCalculator from './prod-calculator.js'
 import { describe, test } from 'vitest'
 
 describe('ProdStringCalculator', () => {
