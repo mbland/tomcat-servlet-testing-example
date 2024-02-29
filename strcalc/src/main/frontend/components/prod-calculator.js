ab2b76a1aa53796fc9166fae215c8bca4f8e7e76
Mike Bland <mbland@acm.org>
2024-02-28 21:45:51 -0500

    Add first version of ProdStringCalculator.add()
    This is the fifth step in the JavaScript Test-Driven Development demo.
    
    The new ProdStringCalculator.add(), hardcoded to only return zero,
    enables the test to build and pass again.
    
    Now we're ready to add the next bit of behavior, which is to handle a
    string containing a single digit.

--- strcalc/src/main/frontend/components/prod-calculator.js
+++ strcalc/src/main/frontend/components/prod-calculator.js
@@ -5,4 +5,13 @@
  */
 
 export default class ProdStringCalculator {
+  /**
+   * Returns the sum of the numbers in the input string.
+   * @see https://osherove.com/tdd-kata-1
+   * @param {string} s - delimited string of numbers to add
+   * @returns {number} - the sum of the numbers in s
+   */
+  add(s) {
+    return 0
+  }
 }
