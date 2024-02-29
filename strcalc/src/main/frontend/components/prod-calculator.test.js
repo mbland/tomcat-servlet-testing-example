f52d85ccc54ff826d1f7162f0b5329aab1394740
Mike Bland <mbland@acm.org>
2024-02-28 21:25:17 -0500

    Add empty ProdStringCalculator test
    This is the first step in the JavaScript Test-Driven Development demo.
    
    I made sure the project built and the existing tests passed first. This
    new, empty test fixture and small test case method also builds and
    passes.
    
    With this infrastructure in place, we're ready to make this test fail
    before adding new production behavior.
    
    Note: There's currently an ESLint warning that the new test has no
    assertions. We'll fix that shortly.

---
+++ strcalc/src/main/frontend/components/prod-calculator.test.js
@@ -0,0 +1,14 @@
+/* eslint-env browser, node, jest, vitest */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+
+import { describe, test } from 'vitest'
+
+describe('ProdStringCalculator', () => {
+  test('empty string returns zero', () => {
+  })
+})
